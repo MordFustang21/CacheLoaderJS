2f8a40c730a4ca28716012fd83b0db308dcfaf8b
Derrick Laird <swampdonk@gmail.com>
2015-12-09 01:01:55 -0700

    initial commit

---
+++ cacheLoader.js
@@ -0,0 +1,134 @@
+(function() {
+
+  'use strict';
+
+  window.cachedLoader = {};
+  
+  var cachedLoader = window.cachedLoader;
+
+  /**
+   * Handles local storage
+   */
+  cachedLoader.storageManager = {
+    putFile: function(key, value) {
+      window.localStorage.setItem(key, value);
+    },
+    getFile: function(key) {
+      return window.localStorage.getItem(key);
+    }
+  };
+
+  /**
+   * Makes XMLHttpRequest for files and returns promise
+   */
+  cachedLoader.loadFile = function(fileName) {
+    var local = this;
+    return new Promise(function(resolve, reject) {
+      var script = local.storageManager.getFile(fileName);
+      if (local.debug) {
+        script = false;
+      }
+      if (script) {
+        resolve(script);
+      } else {
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", function() {
+          local.storageManager.putFile(fileName, this.responseText);
+          resolve(this.responseText);
+        });
+        oReq.open("GET", fileName);
+        oReq.send();
+      }
+    });
+  }
+
+  /**
+   * Handles css Files
+   */
+  cachedLoader.styleList = [];
+  cachedLoader.addStyle = function(styleName) {
+    this.styleList.push(styleName);
+  }
+
+  /**
+   * Handles js Files
+   */
+  cachedLoader.jsList = [];
+  cachedLoader.addJs = function(jsName) {
+    this.jsList.push(jsName);
+  }
+
+  /**
+   * If set gets new version of files
+   */
+  cachedLoader.debug = false;
+  cachedLoader.setDebug = function(bool) {
+    this.debug = bool;
+  }
+
+  /**
+   * Begins load of js list
+   */
+  cachedLoader.loadJs = function() {
+    var local = this;
+    return new Promise(function(resolve, reject) {
+      var promiseList = [];
+      local.jsList.forEach(function(js) {
+        promiseList.push(local.loadFile(js))
+      });
+      Promise.all(promiseList)
+        .then(function(scripts) {
+          scripts.forEach(function(script) {
+            eval(script);
+          });
+          resolve();
+        });
+    });
+  }
+
+  /**
+   * Begins load of style list
+   */
+  cachedLoader.loadStyle = function() {
+    var local = this;
+    return new Promise(function(resolve, reject) {
+      var promiseList = [];
+      local.styleList.forEach(function(style) {
+        promiseList.push(local.loadFile(style));
+      });
+
+      Promise.all(promiseList)
+        .then(function(styles) {
+          styles.forEach(function(style) {
+            var styleTag = document.createElement("style");
+            styleTag.innerHTML = style;
+            document.getElementsByTagName("head")[0].appendChild(styleTag);
+          });
+          resolve();
+        });
+    });
+  }
+
+  /**
+   * Starts load
+   */
+  cachedLoader.load = function() {
+    var stylePromise = this.loadStyle();
+    var jsPromise = this.loadJs();
+
+    return Promise.all([stylePromise, jsPromise]);
+  }
+})();
+
+
+var jsList = ["angular.min.js", "angular-route.min.js", "angular-animate.min.js", "angular-aria.min.js", "angular-messages.min.js", "angular-material.min.js", "app.js"];
+jsList.forEach(function(file) {
+  cachedLoader.addJs('js/' + file);
+});
+
+cachedLoader.addStyle('style/angular-material.min.css');
+
+cachedLoader.load()
+  .then(function() {
+    angular.bootstrap(document, ['isthereawait']);
+  })
